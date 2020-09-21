b8d4d108a5d5870f30fb519bcc3c65529423bee2
luishfb <luisgaijin@gmail.com>
2020-09-21 19:50:14 -0300

    Tutorial 3 - Não funciona Simulação

---
+++ Entrega_3_FPGA_IP/software/FPGA_IP/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c ../FPGA_IP_bsp/system.h \
+ ../FPGA_IP_bsp/linker.h ../FPGA_IP_bsp//HAL/inc/alt_types.h \
+ ../FPGA_IP_bsp//HAL/inc/io.h ../FPGA_IP_bsp//HAL/inc/alt_types.h
+
+../FPGA_IP_bsp/system.h:
+
+../FPGA_IP_bsp/linker.h:
+
+../FPGA_IP_bsp//HAL/inc/alt_types.h:
+
+../FPGA_IP_bsp//HAL/inc/io.h:
+
+../FPGA_IP_bsp//HAL/inc/alt_types.h:
